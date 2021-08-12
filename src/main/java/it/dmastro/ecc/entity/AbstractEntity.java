f85095cfe68380f0f807da4caf10c6d72e2a0ea4
davide_mastroianni <mastroianni.davidedst@gmail.com>
2021-08-12 22:05:08 +0200

    removed version and user attribute from abstract entities

--- src/main/java/it/dmastro/ecc/entity/AbstractEntity.java
+++ src/main/java/it/dmastro/ecc/entity/AbstractEntity.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.beans.factory.annotation.Value;
 
 @Data
 @NoArgsConstructor
@@ -20,20 +19,5 @@ public abstract class AbstractEntity {
 	@Column(name = "update_timestamp")
 	@CreationTimestamp
 	LocalDateTime modifiedDate;
-
-	@Column(name = "insert_user", nullable = false)
-	String creationUser;
-
-	@Column(name = "update_user")
-	String modificationUser;
-
-	@Column(name = "version", nullable = false)
-	String version;
-
-	public AbstractEntity(@Value("${spring.application.name}") String creationUser,
-			@Value("${spring.application.database.version}") String version) {
-		this.creationUser = creationUser;
-		this.version = version;
-	}
 
 }
